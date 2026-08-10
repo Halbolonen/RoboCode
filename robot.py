e7b2580403261f03a5fb3d9515a69ed1a59707db
Halbolonen <halbolonen@gmail.com>
2026-08-10 16:15:06 +0100

    exciting

--- robot.py
+++ robot.py
@@ -2,17 +2,19 @@ from sbot import motors, utils
 
 while True:
     motors.set_power(0, 0.5)
+    motors.set_power(1, 0)
+    utils.sleep(2)
+
     motors.set_power(1, 0.5)
-    utils.sleep(3)
+    utils.sleep(2)
 
     motors.set_power(0, 0)
-    motors.set_power(1, 0)
-    utils.sleep(1.4)
+    utils.sleep(2)
 
-    motors.set_power(0, -0.5)
-    motors.set_power(1, -0.5)
-    utils.sleep(1)
+    motors.set_power(0,0.5)
+    motors.set_power(1,-0.5)
+    utils.sleep(5)
 
-    motors.set_power(0, 0)
-    motors.set_power(1, 0)
-    utils.sleep(4)
+    motors.set_power(0,0)
+    motors.set_power(1,0)
+    utils.sleep(2)
