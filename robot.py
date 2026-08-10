086f7f421d2b352e62a4fc856da7e0c27778e7bd
Halbolonen <halbolonen@gmail.com>
2026-08-10 17:01:28 +0100

    final commit 10/08/26 @ the uni

--- robot.py
+++ robot.py
@@ -1,20 +1,10 @@
 from sbot import motors, utils
 
 while True:
+    # 1 is left 0 is right
     motors.set_power(0, 0.5)
-    motors.set_power(1, 0)
-    utils.sleep(2)
-
-    motors.set_power(1, 0.5)
-    utils.sleep(2)
-
-    motors.set_power(0, 0)
-    utils.sleep(2)
-
-    motors.set_power(0,0.5)
-    motors.set_power(1,-0.5)
+    motors.set_power(1, 0.475)
     utils.sleep(5)
-
     motors.set_power(0,0)
     motors.set_power(1,0)
-    utils.sleep(2)
+    utils.sleep(5)
