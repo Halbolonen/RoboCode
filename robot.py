453c22e8138b43ab303df475ac6748a03b9a02b8
Halbolonen <halbolonen@gmail.com>
2026-08-10 15:01:51 +0100

    basic motor control

--- robot.py
+++ robot.py
@@ -1,1 +1,18 @@
-from sbot import *
+from sbot import motors, utils
+
+while True:
+    motors.set_power(0, 0.5)
+    motors.set_power(1, 0.5)
+    utils.sleep(3)
+
+    motors.set_power(0, 0)
+    motors.set_power(1, 0)
+    utils.sleep(1.4)
+
+    motors.set_power(0, -0.5)
+    motors.set_power(1, -0.5)
+    utils.sleep(1)
+
+    motors.set_power(0, 0)
+    motors.set_power(1, 0)
+    utils.sleep(4)
